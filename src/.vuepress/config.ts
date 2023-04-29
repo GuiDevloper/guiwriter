4cb7f82838731ffef70ce7da38269829d3fded7d
Guilherme Correia <guicorrea384@gmail.com>
2023-04-29 01:46:53 -0300

    :sparkles: Configure `head` tags

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -17,6 +17,37 @@ export default defineUserConfig({
     },
     hostname: ''
   }),
+  head: [
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '120x120',
+        href: '/icons/apple-touch-icon.png'
+      }
+    ],
+    ['link', { rel: 'icon', sizes: '32x32', href: '/icons/favicon-32x32.png' }],
+    ['link', { rel: 'icon', sizes: '16x16', href: '/icons/favicon-16x16.png' }],
+    ['link', { rel: 'manifest', href: '/site.webmanifest' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    [
+      'link',
+      {
+        rel: 'mask-icon',
+        href: '/icons/safari-pinned-tab.svg',
+        color: '#25262b'
+      }
+    ],
+    ['meta', { name: 'msapplication-TileColor', content: '#c44644' }],
+    ['meta', { name: 'theme-color', content: '#c44644' }],
+    [
+      'meta',
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      }
+    ]
+  ],
   locales: {
     '/': {
       lang: 'pt-BR'

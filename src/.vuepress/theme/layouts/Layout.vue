99ba2cb3ed59d1e18eb1dfeb932495f5bbc698db
Guilherme Correia <guicorrea384@gmail.com>
2023-04-29 03:50:36 -0300

    :lipstick: Add global styling to posts listing

--- src/.vuepress/theme/layouts/Layout.vue
+++ src/.vuepress/theme/layouts/Layout.vue
@@ -20,20 +20,52 @@
       </template>
       <Home v-else-if="!frontmatter.blog" />
     </template>
+
+    <template
+      #page-content-top
+      v-if="frontmatter.blog === true"
+    >
+      <p class="blog-date-reading">
+        <time :datetime="frontmatter.date?.toString()">
+          {{ publishDate }}
+        </time>
+        <template v-if="readingTime">
+          {{ ` - ${readingTime.minutes} min de leitura` }}
+        </template>
+      </p>
+      <BlogPostTags
+        v-if="frontmatter.tags"
+        :tags="frontmatter.tags"
+      />
+    </template>
   </ParentLayout>
 </template>
 
 <script setup lang="ts">
 // @ts-ignore
 import ParentLayout from '@vuepress/theme-default/layouts/Layout.vue'
-import { usePageFrontmatter } from '@vuepress/client'
+import { usePageData, usePageFrontmatter } from '@vuepress/client'
 import { computed } from 'vue'
-import { CustomPageFrontmatter } from 'theme/components/view-utils'
-import { Home } from '../components/index.vue'
+import { CustomPageFrontmatter, getPublishDate } from '../components/view-utils'
+import { Home, BlogPostTags } from '../components/index.vue'
+
+type ReadingTimeObject = {
+  readingTime: {
+    minutes: string
+  }
+}
+
+const readingTime = computed(() => {
+  return usePageData<ReadingTimeObject>().value.readingTime
+})
 
 const frontmatter = computed(() => {
   return usePageFrontmatter<CustomPageFrontmatter>().value
 })
 
+const publishDate = computed(() => getPublishDate(frontmatter.value.date))
+
 const image = computed(() => frontmatter.value.Image)
 </script>
+
+<style lang="stylus" src="../styles/index.styl" />
